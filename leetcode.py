63cece1d2479f5792a628a37377a56cecb211052
sqzhang <3257179914@qq.com>
2020-08-15 17:17:50 +0800

    update

--- leetcode.py
+++ leetcode.py
@@ -166,7 +166,8 @@ if __name__ == '__main__':
 
         # 提交到Github仓库
         # 若README.md文件与上次获取的一模一样，则Github不会提交到仓库
-        print(os.popen('git add .', 'r').readlines())
+        # 忽略leetcode.log文件，即不add leetcode.log
+        print(os.popen("git add --all -- ':!leetcode.log'", 'r').readlines())
         print(os.popen('git commit -m "update"', 'r').readlines())
         print(os.popen('git push', 'r').readlines())
 
