8bcc41dca3a58e1566a33d180411f0bbdd65882c
sqzhang <3257179914@qq.com>
2021-02-07 14:58:39 +0800

    revise readme.md

--- leetcode.py
+++ leetcode.py
@@ -101,9 +101,12 @@ def get_accepted_problems(session):
 def generate_markdown_text(response_data, session):
 
 
-    # 部署方法
+    # 相关介绍
     response_data = response_data['data']['userProfileQuestions']['questions']
-    markdown_text =  "## 部署步骤\n"
+    markdown_text =  "## 相关介绍\n"
+    markdown_text += "这是一个简易的LeetCode自动统计程序, 可自动统计最近提交通过的题目, 并以Markdown的形式展示相关的数据。\n"
+    markdown_text += "根据个人需求, 我只重点获取**提交次数**和**重刷次数**这两个指标, 目的是为了更好地辅助做题。\n"
+    markdown_text =  "## 使用教程\n"
     markdown_text += "1. Fork本仓库\n"
     markdown_text += "2. 配置GitHub Actions所需的参数\n"
     markdown_text += "    - 点击仓库下的Settings->Secrets->New repository secret, 分别添加以下secret\n"
@@ -112,7 +115,12 @@ def generate_markdown_text(response_data, session):
     markdown_text += "    - 点击[tokens](https://github.com/settings/tokens)->Generate new token\n"
     markdown_text += "        - Note:GITHUB_TOKEN\n"
     markdown_text += "        - Select scopes:建议全部勾选\n"
-    markdown_text += "\n"
+    markdown_text += "    - 修改[action.yml](.github/workflows/action.yml)文件的第`42行`, 将`email`更改为你的GitHub邮箱地址\n"
+    markdown_text += "    - 修改[action.yml](.github/workflows/action.yml)文件的第`43行`, 将`name`更改为你的GitHub用户名\n"
+    markdown_text += "3. 默认配置为1440分钟(24小时)更新一次，可根据需求修改[action.yml](.github/workflows/action.yml)文件的第`6行`\n"
+    markdown_text =  "## 补充说明\n"
+    markdown_text += "如有其他需求, 欢迎提交PR。\n"
+    markdown_text += "\n\n"
     markdown_text += "> 重刷次数的计算规则为: 累计所有提交通过且互为不同一天的记录次数\n"
     markdown_text += "\n"
     markdown_text += "| 最近提交时间 | 题目 | 题目难度 | 提交次数| 重刷次数 |\n| ---- | ---- | ---- | ---- | ---- |\n"
